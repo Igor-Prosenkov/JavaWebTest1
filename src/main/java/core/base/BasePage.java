d46b05cc14ddcdc97edf84a6164d598b80fe4480
CXPOH <cxpoh1988@gmail.com>
2026-07-27 21:31:23 +0300

    Добавил 3 теста (неверный логин и пароль, только логин и только пароль)+ повторил всё с урока

--- src/main/java/core/base/BasePage.java
+++ src/main/java/core/base/BasePage.java
@@ -1,27 +1,34 @@
 package core.base;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class BasePage {
-    protected SelenideElement headerLogo = $("[tsid='toolbar_logo']");
-    protected SelenideElement seachField =  $("[name='st.query']");
-    protected SelenideElement vkServices =  $("[data-l='t,vk_ecosystem]");
+    protected SelenideElement headerLogo = $("[name='logo-icon']");
+    protected SelenideElement seachField =  $("[name='search-input']");
+    protected SelenideElement Services =  $("[name='header-services-arrow']");
+    protected SelenideElement accertCookieButton = $("[name='cookie-accept-btn']");
 
-   //Метод для поиска по сайту
+  @Step ("Метод для поиска по сайту")
     public void search(String query) {
         seachField.shouldBe(visible).setValue(query).pressEnter();
     }
 
-    //клик по иконке
-    public void openVkservices() {
-        vkServices.shouldBe(visible).click();
+    @Step ("клик по иконке")
+    public void openServices() {
+        Services.shouldBe(visible).click();
     }
 
-    //Клик на логотип ОК
+    @Step ("Клик на логотип CC")
     public void ckickLogo(){
         headerLogo.shouldBe(visible).click();
+    }
+
+    @Step ("Принимаем куки")
+    public void accertCookie(){
+        accertCookieButton.shouldBe(visible).click();
     }
 }
---
+++ src/test/java/tests/LoginTestWithOnlyPassword.java
@@ -0,0 +1,35 @@
+package tests;
+
+
+
+import core.base.BaseTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pages.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LoginTestWithOnlyPassword extends BaseTest {
+    private static LoginPage loginPage;
+
+    @BeforeEach
+            public void prepare() {
+        open(baseUrl);
+        loginPage = new LoginPage();
+        loginPage.accertCookie();
+    }
+
+    @Test
+    public void LoginTestWithOnlyPassword(){
+        loginPage.onlyLogin("fgnfgn");
+        //Проверяем наличие ошибки
+        assertTrue(loginPage.isErrorMessageVisible(),"Сообщение об ошибке входа не отображаются");
+
+        //Проверка текста сообщения об ошибке
+        String expectedErrowMassage = "Введите телефон, email или логин и пароль.";
+        String actualMessage = loginPage.getErrorMessageText();
+        assertEquals(expectedErrowMassage, actualMessage, "текст сообщения не совпадает");
+    }
+}
